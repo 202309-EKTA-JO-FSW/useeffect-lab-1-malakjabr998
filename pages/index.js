aa6b73a472050f25278cd18aa8fda05fa3ef4075
malak jabr <malakjabr2@gmail.com>
2023-10-30 19:29:42 +0300

    Completed assignment

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import DogList from '../Components/DogList/DogList'
 import Form from '../Components/Form/Form'
 import { Inter } from 'next/font/google'
@@ -6,15 +7,21 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  // You will need to put a state here to save all the dogs data into
-  // And you will fetch the data with useEffect
+  const [dogs, setDogs] = useState([])
+   useEffect(()=>{
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+    .then((response)=>response.json())
+    .then((data)=>{
+      setDogs(data.message)
+    })
+   }
 
+   )
    return (
     <div className="card">
-      {/* When the button is clicked in the form, it should fetch the information. 
-          How can we do that by utilizing useState?
-          
-      */}
+      
+        <DogList dogs={DogList.breeds}/> 
+      
       {/* <Form /> Uncomment <Form /> if you are going after the bonus */}
       {/* This page should receive the images array */}
       <DogList />
